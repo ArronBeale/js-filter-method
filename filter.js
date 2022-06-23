a872bb05152efdb19f938c228d720ae0f9628298
Arron <beale86@msn.com>
2022-06-23 17:44:22 +0000

    Add usage example of filter method

--- filter.js
+++ filter.js
@@ -79,3 +79,34 @@ console.log(candidates);
 // map used to return just the names as we only want names
 const names = candidates.map(student => student.name);
 console.log(names);
+
+// ------------------------------------------- Usage
+let students2 = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+const topMaths = students2.filter(p => p.results.maths >= 90);
+console.log(topMaths);
